919f18515fc0600c26754b9c2f993e423666b620
İlkcan Doğan <40579186+IlkcanDogan@users.noreply.github.com>
2019-03-28 23:07:32 +0300

    Add files via upload

---
+++ Sınıflar Arası Kalıtım-Miras Alma (Inheritance)/Kalitim/A.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalitim
+{
+    class A
+    {
+        public int degisken_1;
+
+        public void Metot_1()
+        {
+            Console.WriteLine("A sınıfının Metodu");
+        }
+    }
+
+    class B : A
+    {
+        public int degisken_2;
+
+        public void Metot_2()
+        {
+            Console.WriteLine("B sınfının metodu");
+        }
+    }
+
+    /*
+     * Yukarıdaki örnekte A temel sınıf, B ise türetilmiş sınıftır. B sınıfı kalıtım yolu ile A
+     * sınıfındaki değişken ve metotları kalıtım yolu ile almıştır.
+     */
+}
---
+++ Sınıflar Arası Kalıtım-Miras Alma (Inheritance)/Kalitim/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalitim
+{
+    class Program
+    {
+        /*
+         * Kalıtım (Miras) Nedir?
+         * 
+         * Nesneye dayalı programlamanın en önemli özelliklerinden biridir. Kalıtım yolu ile sınıflar
+         * birbirinden türetilebilir. Bir sınıf diğer bir sınıftan türediği zaman, türediği sınıfın
+         * bütün özelliklerini içerir. Bunun yanında kendisine has özellikeri de barındırabilir.
+         * 
+         */
+        static void Main(string[] args)
+        {
+            B b = new B();
+
+            b.degisken_1 = 10;
+            b.Metot_1();
+
+            b.degisken_2 = 20;
+            b.Metot_2();
+
+
+            /*
+             * Türetme işleminden dolayı B sınıfı, A sınıfına ait tüm özellikleri kendi içinde 
+             * tanımlanmış gibi kullanabilmektedir.
+             * 
+             * [Dikkat]: Türetilmiş sınıf temel sınıf içerisindeki özellikeri kendi içerisinde
+             * tanımlanmış gibi kullanabilir fakat temel sınıf, türetilmiş sınıf içerisindeki
+             * özellikleri kullanamaz!
+             */
+            Console.ReadLine();
+        }
+    }
+}
